8345f961c8085519a9b79c08f4673cef55699880
ALONE BOY <84889578+PRONOI@users.noreply.github.com>
2021-12-16 19:40:44 +0530

    Add files via upload

---
+++ handlers/alive.py
@@ -0,0 +1,88 @@
+import asyncio
+from time import time
+from datetime import datetime
+from modules.config import BOT_USERNAME
+from modules.helpers.filters import command
+from modules.helpers.command import commandpro
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+    
+   
+
+@Client.on_message(command("start") & filters.private & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/e5f8072af8b13ccb1628a.jpg",
+        caption=f"""**
+ ,      
+    ...
+
+  : [  ](https://t.me/The_Superiour_Network)
+  : [  ](https://t.me/World_FriendShip_Zone)
+    : [ ](https://t.me/Simple_Mundaa)
+
+**""",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "       ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+                ]
+                
+           ]
+        ),
+    )
+    
+    
+@Client.on_message(commandpro(["/start", "/alive", "Sumit"]) & filters.group & ~filters.edited)
+async def start(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/30868ddf51d5599e8c777.jpg",
+        caption=f"""""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "", url=f" ")
+                ]
+            ]
+        ),
+    )
+
+
+@Client.on_message(commandpro(["DarkxMusic","Sumit", "#Channel", "@Channel", "/Channel", "Channel"]) & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/e5f8072af8b13ccb1628a.jpg",
+        caption=f"""""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "", url=f" ")
+                ]
+            ]
+        ),
+    )
